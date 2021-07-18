e149b8033f08c7909f38d438904c6c5913da78a2
Vikram GK <gkvikram95@gmail.com>
2021-07-18 12:08:08 +0800

    part1.9 done

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -3,25 +3,30 @@ const Statistics = (props) => {
     return (
         <div>
             <h1>statistics</h1>
-
-            <p>
-                good {props.good}
-            </p>
-            <p>
-                neutral {props.neutral}
-            </p>
-            <p>
-                bad {props.bad}
-            </p>
-            <p>
-                all {props.allFeedback}
-            </p>
-            <p>
-                average {props.averageFeedback}
-            </p>
-            <p>
-                positive {props.positiveFeedback}
-            </p>
+            {props.allFeedback > 0 ?
+                <div>
+                    <p>
+                        good {props.good}
+                    </p>
+                    <p>
+                        neutral {props.neutral}
+                    </p>
+                    <p>
+                        bad {props.bad}
+                    </p>
+                    <p>
+                        all {props.allFeedback}
+                    </p>
+                    <p>
+                        average {props.averageFeedback}
+                    </p>
+                    <p>
+                        positive {props.positiveFeedback}
+                    </p>
+                </div>
+                :
+                <p>No feedback given.</p>
+            }
         </div>
     )
 }
