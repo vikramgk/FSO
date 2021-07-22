837778345b53931bb18f2e6eb8650ed007ea75a8
Vikram GK <gkvikram95@gmail.com>
2021-07-22 14:16:53 +0800

    part2.14 done

--- part2/countries/src/components/CountryDetails.js
+++ part2/countries/src/components/CountryDetails.js
@@ -1,4 +1,19 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const CountryDetails = ({ country }) => {
+    const REACT_APP_WEATHER_API_KEY = process.env.REACT_APP_WEATHER_API_KEY
+    const [weather, setWeather] = useState({})
+
+    useEffect(() => {
+        axios.get(`http://api.weatherstack.com/current?access_key=${REACT_APP_WEATHER_API_KEY}&query=${country.name}`)
+            .then(response => {
+                console.log(JSON.stringify(response.data, null, 2))
+                setWeather(response.data.current)
+            })
+    }, [])
+
+
     return (
         <div>
             <h1>{country.name}</h1>
@@ -13,6 +28,18 @@ const CountryDetails = ({ country }) => {
             <div>
                 <img alt='flag' height="50px" src={country.flag} />
             </div>
+            {Object.keys(weather).length != 0 ? <div><h2>weather in {country.name}</h2>
+                <img alt='weather-icon' src={weather.weather_icons} />
+                <div>
+                    temperature: {JSON.stringify(weather.temperature)} degrees
+                </div>
+                <div>
+                    uv index: {JSON.stringify(weather.uv_index)}
+                </div>
+            </div>
+                : <p>Loading weather</p>
+            }
+
         </div>
     )
 }
